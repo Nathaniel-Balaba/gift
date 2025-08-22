25f512ad9b872360e1ff0afbe43768d2b772f094
Nathaniel-Balaba <nathanielbalaba1@gmail.com>
2025-08-22 17:56:14 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+onload = () => {
+  const c = setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+    clearTimeout(c);
+  }, 1000);
+
+  const titleElement = document.getElementById('title');
+  if (titleElement) {
+    const text = `FOR YOU JULIE`.split('')
+    titleElement.innerHTML = ''; // Clear existing text
+    
+    for (let index = 0; index < text.length; index++) {
+      if (text[index] !== ' ') {
+        titleElement.innerHTML += `<span>${text[index]}</span>`
+      } else {
+        titleElement.innerHTML += `<span class='space'>&nbsp;</span>`
+      }
+    }
+    
+    const textElements = document.querySelectorAll('#title span');
+    textElements.forEach((element, i) => {
+      element.style.animationDelay = `${i * 0.1}s`;
+    });
+  }
+}; 
